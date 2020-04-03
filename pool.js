2fff589946658e349b3b758c1ba7eb785fea7153
Neithan <nathangomes@mat.ci.ufpb.br>
2020-04-02 23:48:49 -0300

    pool

--- pool.js
+++ pool.js
@@ -3,13 +3,8 @@ const pool = new Pool({
     host: '127.0.0.1',
     port: 5432,
     user: 'postgres',
-<<<<<<< HEAD
     password: '123456',
     database: 'postgres2'
-=======
-    password: '12345678',
-    database: 'postgres3'
->>>>>>> e6a541a8c2fd4301f7c779d2cef8cd8380812743
     },
     API_PORT=3001
 )
